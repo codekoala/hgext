26f8ce32bda358bd4158ca02a07b1bc803a4b455
Josh VanderLinden <jvanderlinden@sciencelogic.com>
2009-12-16 12:18:08 -0500

    Updated the mpush extension to include an mpull extension to do the same sort of thing

--- mpush.py
+++ mpush.py
@@ -6,8 +6,13 @@ def multi_push(ui, repo, **opts):
     for name, location in ui.configitems('paths'):
         commands.push(ui, repo, location, **opts)
 
+def multi_pull(ui, repo, **opts):
+    """Pulls changesets in from all repos listed in the [paths] section of the config"""
+
+    for name, location in ui.configitems('paths'):
+        commands.pull(ui, repo, location, **opts)
+
 cmdtable = {
-    '^mpush': (multi_push, 
-    commands.table['^push'][1], 
-    commands.table['^push'][2].replace('[DEST]', ''))
+    '^mpush': (multi_push, commands.table['^push'][1], commands.table['^push'][2].replace('[DEST]', '')),
+    '^mpull': (multi_pull, commands.table['^pull'][1], commands.table['^pull'][2].replace('[SOURCE]', ''))
 }
